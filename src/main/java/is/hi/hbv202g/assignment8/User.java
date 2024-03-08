2e31271c6b4de35b4423dbf32f74a63770bde8e9
Brynjar <brynjar.sighvatsson@gmail.com>
2024-03-08 09:18:48 +0000

    Added Abstract class User and child classes

--- src/main/java/is/hi/hbv202g/assignment8/User.java
+++ src/main/java/is/hi/hbv202g/assignment8/User.java
@@ -1,6 +1,17 @@
 package is.hi.hbv202g.assignment8;
-public class User {
-    public static void main(String[] args) {
-        
+public abstract class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
