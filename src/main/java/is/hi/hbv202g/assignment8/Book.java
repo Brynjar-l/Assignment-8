a718af26f082db53f55918097fa536d18b45eaf9
Brynjar <brynjar.sighvatsson@gmail.com>
2024-03-08 09:50:50 +0000

    Started working on the Book.java

--- src/main/java/is/hi/hbv202g/assignment8/Book.java
+++ src/main/java/is/hi/hbv202g/assignment8/Book.java
@@ -1,6 +1,25 @@
 package is.hi.hbv202g.assignment8;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
-    public static void main(String[] args) {
-        
+    
+    private String title;
+    private List<Author> authors = new ArrayList<Author>();
+
+    public Book(String title, String authorName) { // No need for Exception throwing
+        this.title = title;
+        authors.add(new Author(authorName));
     }
+    
+    public Book(String title, List<Author> authors) throws EmptyAuthorListException {
+        if (authors.isEmpty()) {
+            throw new EmptyAuthorListException("EmptyAuthorList");
+        }
+        this.title = title;
+        this.authors = authors;
+    }
+
+
+
 }
